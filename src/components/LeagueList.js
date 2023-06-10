7d455d1e2339910d8176418a5d1c7d769b142b40
trichomer <a@b.c>
2023-06-10 10:55:43 -0600

    Starting on LeagueList component

---
+++ src/components/LeagueList.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function LeagueList() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default LeagueList;
